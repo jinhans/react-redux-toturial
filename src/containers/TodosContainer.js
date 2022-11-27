65c5b47eb06a2106dd57323b03fa3538b35fa5d1
jinhans <wlsgks846@gmail.com>
2022-11-27 14:24:39 +0900

    성능최적화
    를 위해 React.memo를 컨테이너 컴포넌트에 사용

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../components/Todos';
@@ -17,7 +18,7 @@ const TodosContainer = () => {
     <Todos
       input={input}
       todos={todos}
-      onChangeInput={changeInput}
+      onChangeInput={onChangeInput}
       onInsert={onInsert}
       onToggle={onToggle}
       onRemove={onRemove}
@@ -25,4 +26,4 @@ const TodosContainer = () => {
   );
 };
 
-export default TodosContainer;
+export default React.memo(TodosContainer);
